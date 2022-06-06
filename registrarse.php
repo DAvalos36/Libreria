b85c2e1a15022200ea02ef37f06f0d90e791b4c0
David Avalos <davalos@itparral.edu.mx>
2022-06-06 08:34:59 -0600

    Css en carpeta

--- registrarse.php
+++ registrarse.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="si.css">
+    <link rel="stylesheet" href="css/si.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
